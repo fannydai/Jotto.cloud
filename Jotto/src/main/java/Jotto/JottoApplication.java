0cdfa973e7885d02cfd6cdc456181c5e3e0ecd1e
Qi Yuan Fang <qiyuan.fang@stonybrook.edu>
2019-02-12 23:09:37 -0500

    Basic File Structure for backend Spring Boot Server, using Maven for build service, dependencies added: Web, REST Repositories, Security, MongoDB, Session (Security)

---
+++ Jotto/src/main/java/Jotto/JottoApplication.java
@@ -0,0 +1,14 @@
+package Jotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JottoApplication.class, args);
+	}
+
+}
+
