932fa6ebd015c1b1787ef64f5514771abae9b5da
Qi Yuan Fang <qiyuan.fang@stonybrook.edu>
2019-02-13 00:15:52 -0500

    Finished Maven build service work, run with mvn spring-boot :run

---
+++ Jotto/src/main/java/server/Jotto/JottoApplication.java
@@ -0,0 +1,14 @@
+package server.Jotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JottoApplication.class, args);
+	}
+
+}
+
---
+++ Jotto/src/test/java/server/Jotto/JottoApplicationTests.java
@@ -0,0 +1,17 @@
+package server.Jotto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JottoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
