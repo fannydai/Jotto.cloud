3636d5b5351861be6771b5a936c1c819af9a5019
Qi Yuan Fang <qiyuan.fang@stonybrook.edu>
2019-02-14 16:57:09 -0500

    Added web security config for Spring - security, allowing index page, login, register to be unauthenticated access, added User model and User mongodb repository, added some work in progress controllers

--- Jotto/src/main/java/server/Jotto/JottoApplication.java
+++ Jotto/src/main/java/server/Jotto/JottoApplication.java
@@ -9,15 +9,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+
+import server.Jotto.Models.UserRepository;
+
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@Controller
-@Configuration
-@ComponentScan(basePackageClasses = UserRestController.class)
 public class JottoApplication {
+
+    @Autowired
+    private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JottoApplication.class, args);
@@ -27,20 +37,21 @@ public class JottoApplication {
     @Configuration
     protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-            http
-                .httpBasic().and()
-                .authorizeRequests()
-                    .antMatchers("/index", "/", "/register", "/login").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .csrf()
-                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-            // @formatter:on
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new CorsSupport(), ChannelProcessingFilter.class);
+        http
+        .authorizeRequests()
+        .antMatchers("/","/register")
+        .permitAll()
+        .and()
+.authorizeRequests()
+        .anyRequest()
+        .authenticated()
+        .and().csrf().disable();
+    }
 		}
-	}
+    }
 
 
-}
+
 
