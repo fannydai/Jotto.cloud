0b833ae6ba28cd581d8f281245efe3254651b10a
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-02-15 00:12:53 -0500

    Fixed register, updated user service

--- front-end/src/app/services/user.service.ts
+++ front-end/src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { of, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +13,37 @@ export class UserService {
     private http: HttpClient
   ) { }
 
-  login(username: string, password: string) {
-    return this.http.post<any>(`${environment.api_path}/login`, { username: username, password: password });
+  /**
+   * Login user using POST request to back-end server.
+   *
+   * @param username - name of user
+   * @param password - password of user
+   */
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(`${environment.api_path}/login`, { username: username, password: password })
+      .pipe(
+        map(result => {
+          console.log(result);
+          return result;
+        }),
+        catchError(err => null)
+      );
   }
 
+  /**
+   * Register user using POST request to back-end server.
+   *
+   * @param username - name of user
+   * @param password - password of user
+   */
   register(username: string, password: string) {
-    return this.http.post<any>(`${environment.api_path}/register`, { username: username, password: password });
+    return this.http.post<any>(`${environment.api_path}/register`, { username: username, password: password })
+      .pipe(
+        map(result => {
+          console.log(result);
+          return result;
+        }),
+        catchError(err => null)
+      );
   }
 }
