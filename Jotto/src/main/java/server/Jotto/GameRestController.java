c14096145e0da58fd282e8d0108a3af5007d013d
Qi Yuan Fang <qiyuan.fang@stonybrook.edu>
2019-02-17 15:54:35 -0500

    Created GameRestController skeleton file for game rest controller logic

---
+++ Jotto/src/main/java/server/Jotto/GameRestController.java
@@ -0,0 +1,14 @@
+package server.Jotto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.*;
+import java.security.Principal;
+import server.Jotto.Models.*;
+
+@RestController
+public class GameRestController{
+
+}
