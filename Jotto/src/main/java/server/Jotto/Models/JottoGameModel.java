149f8e8647a61b85178c65ad5141daf72f25747d
Qi Yuan Fang <qiyuan.fang@stonybrook.edu>
2019-02-17 15:52:33 -0500

    Added code for JottoGameModel, JottoMoveModel, logic still needs work for game

---
+++ Jotto/src/main/java/server/Jotto/Models/JottoGameModel.java
@@ -0,0 +1,52 @@
+package server.Jotto.Models;
+
+import java.util.ArrayList;
+/**
+ * POJO representation of Jotto game
+ */
+public class JottoGameModel{
+    private String answerword;
+    private ArrayList<JottoMoveModel> moves;
+    private int moveamount;
+    private final int moveLimit;
+
+    public JottoGameModel(){
+        this.moveLimit = 15;
+        this.answerword = generateAnswerWord();
+        this.moveamount = 0;
+        this.moves = new ArrayList<JottoMoveModel>();
+
+    }
+
+    public boolean evaluate(){
+        return true;
+    }
+
+    private String generateAnswerWord(){
+        return "Answer";
+    }
+
+    public void addNewMove(JottoMoveModel newMove){
+        this.moves.add(newMove);
+    }
+
+    public String getAnswerWord(){
+        return this.answerword;
+    }
+    public void setAnswerWord(String newAnswerWord){
+        this.answerword = newAnswerWord;
+    }
+    public ArrayList<JottoMoveModel> getMoves(){
+        return this.moves;
+    }
+    public void setMoves(ArrayList<JottoMoveModel> newMoveList){
+        this.moves = newMoveList;
+        this.moveamount = moves.size();
+    }
+    public int getMoveAmount(){
+        return this.moveamount;
+    }
+    public int getMoveLimit(){
+        return this.moveLimit;
+    }
+}
---
+++ Jotto/src/main/java/server/Jotto/Models/JottoMoveModel.java
@@ -0,0 +1,9 @@
+package server.Jotto.Models;
+
+
+/**
+ * POJO model representation of move in Jotto game.
+ */
+public class JottoMoveModel{
+    String guessedword;
+}
