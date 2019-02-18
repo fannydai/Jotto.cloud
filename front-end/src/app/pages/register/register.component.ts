1b764b17671727e8e73e89c299a323fbb9f82805
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-02-18 17:33:59 -0500

    Register requirements info

--- front-end/src/app/pages/register/register.component.ts
+++ front-end/src/app/pages/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +11,6 @@ export class RegisterComponent implements OnInit {
   private username = '';
   private password = '';
   private errorMessage = '';
-  // registerForm: FormGroup;
 
   constructor(
     private user: UserService,
@@ -20,18 +18,6 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    /*
-    this.registerForm = new FormGroup({
-      'username': new FormControl(this.username, [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-      'password': new FormControl(this.password, [
-        Validators.required,
-        Validators.minLength(10),
-        this.checkPassword()
-      ])
-    });*/
   }
 
   onSubmit(): void {
@@ -57,21 +43,6 @@ export class RegisterComponent implements OnInit {
     const hasLower = /[a-z]/.test(password);
     const hasDigit = /[0-9]/.test(password);
     const hasSymbol = /[!%#$&()*+,-./:;<=>?@[\]^_`{|}~]/.test(password);
-    console.log("hasUpper ", hasUpper);
-    console.log("hasLower ", hasLower);
-    console.log("hasDigit ", hasDigit);
-    console.log("hasSymbol ", hasSymbol);
     return hasUpper && hasLower && hasDigit && hasSymbol;
   }
-  /*
-  checkPassword(): ValidatorFn {
-    console.log('hello');
-    return (control: AbstractControl): {[key: string]: any} | null => {
-      const hasUpper = /[A-Z]/.test(control.value);
-      const hasLower = /[a-z]/.test(control.value);
-      const hasDigit = /[0-9]/.test(control.value);
-      const hasSymbol = /[!%#$&()*+,-./:;<=>?@[\]^_`{|}~]/.test(control.value);
-      return hasUpper && hasLower && hasDigit && hasSymbol ? null : {'passwordError': {value: control.value}};
-    };
-  }*/
 }
