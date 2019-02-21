d63968d26fe80ba7780ddb8b1291f0dabf43cc18
Fanny Dai <fannydai@gmail.com>
2019-02-20 22:10:31 -0500

    Optimized GameRestController + naming convention

--- Jotto/src/main/java/server/Jotto/GameRestController.java
+++ Jotto/src/main/java/server/Jotto/GameRestController.java
@@ -1,15 +1,10 @@
 package server.Jotto;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
-import java.security.Principal;
-import server.Jotto.Models.*;
 
 @RestController
 public class GameRestController{
+    private int size = 5;
     //add decorators and spring shit
     // finish this class and add more models
 
@@ -23,28 +18,34 @@ public class GameRestController{
         //check winner
     }
 
-    public boolean ValidateWord(String to_check){
-        if(to_check.length() == 5){
-            for(int i=0; i<to_check.length(); i++){
-                for(int j=i+1; j<to_check.length(); j++){
-                    if(to_check.charAt(i) == to_check.charAt(j))
-                        return false;
-                }
+    /*
+     * Checks to ensure passed in parameter is exactly length 5 and all leters are unique.
+     * @param word      String which the user typed & needs to be checked
+     * @return          true if word is length 5 and all letters are unique (case insensitive)
+     */
+    public boolean validateWord(String word) {
+        boolean[] letters = new boolean[26];
+        if(word.length() == size) {
+            word = word.toUpperCase();
+            for(int i=0; i<size; i++) {
+                if(letters[word.charAt(i)-65] == true)
+                    return false;
+                else
+                    letters[word.charAt(i)-65] = true;
             }
             return true;
         }
-        else
-            return false;
+        return false;
     }
 
-    public boolean CheckWinner(String userGuess, String compWord){
+    public boolean checkWinner(String userGuess, String compWord){
         return userGuess.equals(compWord);
     }
 
     // @CrossOrigin(origins = "http://localhost:4200")
     // @RequestMapping(value = "/playPage", method = RequestMethod.POST, consumes = {"application/json"}) //MAKE A PAGE FOR THE ACTUAL GAME
     // @ResponseBody
-    public void ShowGuesses(){ //of current game of user and comp **add current user as arg**
+    public void showGuesses(){ //of current game of user and comp **add current user as arg**
         //go through db and get guesses
         //cal num matching letters
     }
@@ -52,27 +53,40 @@ public class GameRestController{
     // @CrossOrigin(origins = "http://localhost:4200")
     // @RequestMapping(value = "/", method = RequestMethod.POST, consumes = {"application/json"}) //index page??
     // @ResponseBody
-    public void ShowPastGameResults(){ //add user as arg
+    public void showPastGameResults(){ //add user as arg
         //go thorugh db and shows games results
     }
 
-    public int CalNumMatchingLetters(String guess, String realWord){
-        int numMatches = 0;
-        for(int i=0; i<realWord.length(); i++){
-            for(int j=0; j<guess.length(); j++){
-                if((realWord.charAt(i) == guess.charAt(j)) && 
-                    (guess.lastIndexOf(guess.charAt(j)) == realWord.lastIndexOf(realWord.charAt(j)))){ //second statement checks for duplicate letters
-                    numMatches+=1;
-                }
+    /*
+     * Counts the amount of matching letters between the actual and guessed word
+     * 
+     * @param guess     The word which was guessed
+     * @param actual    The actual word
+     * 
+     * @return          The total amount of letters that overlap btw guess & actual
+     */
+    public int amtMatch(String guess, String actual){
+        int counter = 0;
+        boolean[] letters = new boolean[26];
+        
+        guess = guess.toUpperCase();
+        actual = actual.toUpperCase();
+        
+        for(int i=0; i<size; i++) {
+            letters[actual.charAt(i)-65] = true;
+        }
+        for(int i=0; i<size; i++) {
+            if(letters[guess.charAt(i)-65] == true) {
+                counter++;
             }
         }
-        return numMatches;
+        return counter;
     }
 
     // @CrossOrigin(origins = "http://localhost:4200")
     // @RequestMapping(value = "/playPage", method = RequestMethod.POST, consumes = {"application/json"}) //MAKE A PAGE FOR THE ACTUAL GAME
     // @ResponseBody
-    public void CalcCompGuess(@RequestBody String prevUserGuess){ //change arg for later (user)
+    public void calcCompGuess(@RequestBody String prevUserGuess){ //change arg for later (user)
 
     }
 }
