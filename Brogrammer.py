cc67c3efb50c92dd4e34131b16120b01efcfa1b5
Fanny Dai <fannydai@gmail.com>
2019-02-23 18:57:01 -0500

    dictionary made & put into directory

---
+++ Brogrammer.py
@@ -0,0 +1,5 @@
+# This program will filter out words in the temp_words.txt
+# file so all words are valid
+
+def filter():
+    file_path = "Jotto⁩/src⁩/main⁩/java⁩/server⁩/Jotto⁩/Models⁩/dictionary.txt"
