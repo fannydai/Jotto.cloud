2e00a831ed3e17f8e0b333613565a86b752d6299
Fanny Dai <fannydai@gmail.com>
2019-02-24 16:19:54 -0500

    pushing unique_char for Joel to finish

--- Jotto/src/main/java/server/Jotto/Models/JottoGameModel.java
+++ Jotto/src/main/java/server/Jotto/Models/JottoGameModel.java
@@ -1,6 +1,8 @@
 package server.Jotto.Models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
 import java.util.regex.*;
 import org.springframework.data.annotation.Id;
 
@@ -17,15 +19,20 @@ public class JottoGameModel {
     private JottoMoveModel userMoves;
     private JottoMoveModel botMoves;
 
+    // Words that the bot has to calclate
+    private HashMap<String,Integer> botWords;
+
     // A list of words that the bot can guess.
     private ArrayList<String> botDict;
     // false means we don't know if is in word or not. True means it is either in word or not (We don't care bc we modify the dictionary as we figure it out)
     private Boolean[] botLetters;
-
+    
     public JottoGameModel(String answerWord, ArrayList<String> botDict) {
         this.size = 5;
-        botLetters = new Boolean[26];           // Default value is set to false
         this.botDict = botDict;
+
+        botLetters = new Boolean[26];
+        Arrays.fill(botLetters, Boolean.FALSE);
 
         this.userMoves = new JottoMoveModel(generateAnswerWord(), this.size);
         this.botMoves = new JottoMoveModel(answerWord, this.size);
@@ -52,37 +59,60 @@ public class JottoGameModel {
      */
     /*
      * Removes all possible guess words for the bot with that certain char -- THIS WAS TESTED AND WORKS -- FANNY
+     * 
      * @param character     Takes in a char and removes words with/without that char
      * @param flag          True = character is in the word. Find all words that do not have it and remove it
      *                      False = character is not in the word. Find all words that have this char and remove it
      */
-    private void removeWord(char[] chars, boolean flag) {
+    private void removeWord(String str, boolean flag) {
         ArrayList<String> tempBotWordList = new ArrayList<String> ();
 
         // Make my regex for letters to remove
         String regex = "";
-        for(char c : chars) {
+        for(char c : str.toCharArray()) {
             if(botLetters[(int)c-65]==false) {
                 botLetters[(int)c-65] = true;
                 regex += c;
             }
         }
         if(regex.length()==0) return;
-        regex = "\"[" + regex + "] \"";
+        regex = "\\w*[" + regex + "]+\\w*";
 
         for(String dictWord : botDict) {
-            if(Pattern.matches(dictWord, regex) == flag) {
+            if(Pattern.matches(regex, dictWord) == flag) {
                 tempBotWordList.add(dictWord);
             }
 		}
 		botDict = tempBotWordList;
     }
 
+    /*
+     * Find the common characters that both str1 & str2 has. Return those values as a string.
+     * 
+     * @param str1      String1 to look for common letters
+     * @param str2      String2 to look for common letters
+     * 
+     * @return          A String which each chars are in both str1 and str2
+     */
+    private String unique_char(String str1, String str2) {
+        // boolean[] letters = new boolean[26];
+        
+        // for(int i=0; i<size; i++) {
+        //     letters[this.word.charAt(i)-65] = true;
+        // }
+        // for(int i=0; i<size; i++) {
+        //     if(letters[guess.charAt(i)-65] == true) {
+        //         counter++;
+        //     }
+        // }
+
+    }
+
     private void botLogic(String guess) {
         int amtMatch = botMoves.addGuessWord(guess);
-        switch (amtMatch) {
+        switch(amtMatch) {
             case 0:
-                // 0 - loop through the entire word and call removeWord(char, false)
+                removeWord(guess, false);
                 break;
             case 1:
                 break;
@@ -93,9 +123,14 @@ public class JottoGameModel {
             case 4:
                 break;
             case 5:
-                // 5 - check if won. If not, loop through word & call removeWord(char, true)
+                if(botMoves.wong()) {
+                    // Bot won
+                } else {
+                    removeWord(guess, true);
+                }
                 break;
             default:
                 // there was an error with the word
         }
+    }
 }
