f3b0873f5e600c7ef99d5a5609d9cafc573df688
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-02-24 20:51:22 -0500

    Worked on game results

--- front-end/src/app/pages/results/results.component.ts
+++ front-end/src/app/pages/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-results',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
+  private errorMessage = '';
+  private playerMoves = [];
+  private computerMoves = [];
 
-  constructor() { }
+  constructor(
+    private game: GameService
+  ) { }
 
   ngOnInit() {
+    this.game.getPreviousGame()
+      .subscribe(res => {
+        if (res === null) {
+          this.errorMessage = 'Server is down.';
+        } else if (res.status === 'success') {
+
+        } else {
+          this.errorMessage = 'No past game results.';
+        }
+      });
   }
 
 }
--- front-end/src/app/services/game.service.ts
+++ front-end/src/app/services/game.service.ts
@@ -1,39 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  // private wordList = [];
 
   constructor(
     private http: HttpClient
-  ) {
-    /*
-    // Load in valid words
-    this.loadWords().subscribe(data => {
-      this.wordList = data.body.split('\n');
-      this.wordList = this.wordList.filter(word => word !== '');
-      this.wordList = this.wordList.map(word => word.trim());
-    });*/
-  }
-
-  /*
-  loadWords(): Observable<any> {
-    return this.http.get('../../assets/dictionary.txt', {observe: 'response', responseType: 'text'});
-  }
-
-  getWords(): any[] {
-    return this.wordList;
-  }
-
-  validateWord(word: string): boolean {
-    word = word.toUpperCase();
-    return this.wordList.includes(word);
-  }*/
+  ) {}
 
   /**
    * Picks the word for the AI to start the game. Backend should make sure word is valid.
@@ -53,5 +31,16 @@ export class GameService {
   guess(word: string): Observable<any> {
     return this.http.post<any>(`${environment.api_path}/playGame`,
       { guess: word.toUpperCase(), username: localStorage.getItem('username') });
+  }
+
+  /**
+   * Gets the previous game associated with a user.
+   */
+  getPreviousGame(): Observable<any> {
+    return this.http.post<any>(`${environment.api_path}/previousGame`, { username: localStorage.getItem('username') })
+      .pipe(
+        map(res => res),
+        catchError(e => of(null));
+      )
   }
 }
