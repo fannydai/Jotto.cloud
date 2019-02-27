fa7bc54bc8b41dde650ad07a94216ce589a2402b
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2019-02-27 15:53:09 -0500

    Forgot to push BotMoveResult

---
+++ Jotto/src/main/java/server/Jotto/Models/BotMoveResult.java
@@ -0,0 +1,17 @@
+package server.Jotto.Models;
+
+public class BotMoveResult {
+    private String result;
+
+    public BotMoveResult() {
+
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+
+    public void setResult(String res) {
+        this.result = res;
+    }
+}
